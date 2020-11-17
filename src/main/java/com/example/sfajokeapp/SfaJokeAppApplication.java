f4d57d805042e462756b770995c768936697affa
Polishyankee <stasicakamil@hotmail.com>
2020-11-17 17:01:01 +0100

    init add of project

---
+++ src/main/java/com/example/sfajokeapp/SfaJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfajokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfaJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfaJokeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sfajokeapp/SfaJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfajokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfaJokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
